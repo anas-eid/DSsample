b1e6f5d6015fcdfa2ef8175756931d27a607d89c
Anas Eid <anas.eid@harri.com>
2022-08-18 10:26:04 +0300

    Create interface display function

---
+++ Git/Main.java
@@ -0,0 +1,57 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private Automobile auto;
+
+    /**
+     * Constructor for objects of class Main
+     */
+    public Main()
+    {
+        // initialise instance variables
+        x = 0;
+        auto = new Automobile();
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    public void displayMainInterface (){
+        int choice = 0;
+        switch(choice){
+            case 0:
+                {
+                    
+                }
+                break;
+            case 1: 
+                {
+                    
+                }
+                break;
+                
+            default:
+                {
+                    
+                }
+                break;
+        }
+    }
+}
